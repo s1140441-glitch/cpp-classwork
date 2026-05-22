e8211240b6ac843243586d603252e0526a79f781
s1140441-glitch <s1140441@dmhs.kh.edu.tw>
2026-05-22 10:54:43 +0800

    Create escape.cpp

---
+++ escape.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout <<"1.再見" << endl; 
+	cout <<"2.再 見" << endl; 
+	cout <<"3.再\t見" << endl; 
+	cout <<"4.再\n見" << endl; 
+	cout <<"\5.再見\"" << endl; 
+	cout <<"\'6.再見\'" << endl; 
+	cout <<"\\7.再見\\" << endl; 
+	return 0; 
+}
